3ede0994eb8c30ead367b29317d7d7784e50ee57
Jie Chen <jiechenmc@gmail.com>
2023-12-13 22:09:27 -0500

    init ssh

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "golang.org/x/crypto/ssh"
 
+func main() {
+	config := &ssh.ClientConfig{
+		User: "user",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("pass"),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, _ := ssh.Dial("tcp", "remotehost:22", config)
+	defer client.Close()
 }
