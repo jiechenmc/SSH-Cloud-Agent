4c5fac881f68290023e50198c23ec16622afdb14
Jie Chen <jiechenmc@gmail.com>
2024-04-23 22:51:18 -0400

    updates

--- files/up.sh
+++ files/up.sh
@@ -1,37 +1,39 @@
 # sudo find . -mtime +1 -type f -name "*" -delete
 # Add Docker's official GPG key:
 
-command -v docker &> /dev/null
+# command -v docker &> /dev/null
 
 
-if [ $? -eq 0 ]; then
-  echo "Copy successful"
-else
-   sudo apt-get update
-    sudo apt-get install ca-certificates curl gnupg -y
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# if [ $? -eq 0 ]; then
+#   echo "Copy successful"
+# else
+#    sudo apt-get update
+#     sudo apt-get install ca-certificates curl gnupg -y
+#     sudo install -m 0755 -d /etc/apt/keyrings
+#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+#     sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-    # Add the repository to Apt sources:
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-fi
+#     # Add the repository to Apt sources:
+#     echo \
+#     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#     sudo apt-get update
+#     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# fi
 
-command -v screen &> /dev/null
+# command -v screen &> /dev/null
 
-if [ $? -eq 0 ]; then
-  echo "Copy successful"
-else
-   sudo apt-get update
-   sudo apt install screen -y
-fi
+# if [ $? -eq 0 ]; then
+#   echo "Copy successful"
+# else
+#    sudo apt-get update
+#    sudo apt install screen -y
+# fi
    
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-sudo docker compose down
-sudo docker compose up --build -d
-sudo docker system prune -f
+# sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+# sudo docker compose down
+# sudo docker compose up --build -d
+# sudo docker system prune -f
+
+sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt-get install -y libuv1-dev
--- main.go
+++ main.go
@@ -22,48 +22,43 @@ func main() {
 	check(err)
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
+	var wg sync.WaitGroup
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		user := strings.Split(line, "@")[0]
 		ip := strings.Split(line, "@")[1]
 
-		ssh, err := core.NewSshClient(
-			user,
-			ip,
-			22,
-			os.Getenv("HOME")+"/.ssh/id_rsa",
-			"")
-
-		check(err)
-
-		files, err := os.ReadDir("./files")
-		check(err)
-		var wg sync.WaitGroup
-
-		for _, file := range files {
-			wg.Add(1)
-			go func(file fs.DirEntry) {
-				srcFilePath := "./files/" + file.Name()
-				remoteFilePath := file.Name()
-				_, err = ssh.CopyFile(srcFilePath, remoteFilePath)
-				if err != nil {
-					fmt.Printf("%s@%s %s\n", user, ip, err)
-				} else {
-					fmt.Printf("%s@%s\t%s -> %s GOOD\n", user, ip, srcFilePath, remoteFilePath)
-				}
-
-				defer wg.Done()
-			}(file)
-		}
-		wg.Wait()
 		wg.Add(1)
 		go func() {
+			ssh, err := core.NewSshClient(
+				user,
+				ip,
+				22,
+				os.Getenv("HOME")+"/.ssh/id_rsa",
+				"")
+
+			check(err)
+			files, err := os.ReadDir("./files")
+			check(err)
+			for _, file := range files {
+				go func(file fs.DirEntry) {
+					srcFilePath := "./files/" + file.Name()
+					remoteFilePath := file.Name()
+					_, err = ssh.CopyFile(srcFilePath, remoteFilePath)
+					if err != nil {
+						fmt.Printf("%s@%s %s\n", user, ip, err)
+					} else {
+						fmt.Printf("%s@%s\t%s -> %s GOOD\n", user, ip, srcFilePath, remoteFilePath)
+					}
+
+				}(file)
+			}
 			ssh.RunCommand("sudo ./up.sh")
 			defer wg.Done()
 		}()
-		wg.Wait()
 	}
+	wg.Wait()
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
