2e128c884a1664fa5e1ff0db4be350da7ee548c5
Jie Chen <jiechenmc@gmail.com>
2024-05-02 10:23:02 -0400

    upload

--- files/up.sh
+++ files/up.sh
@@ -1,40 +1,22 @@
-# sudo find . -mtime +1 -type f -name "*" -delete
+## Installing Docker
+sudo apt update && sudo apt upgrade -y
 # Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# command -v docker &> /dev/null
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
-# if [ $? -eq 0 ]; then
-#   echo "Copy successful"
-# else
-#    sudo apt-get update
-#     sudo apt-get install ca-certificates curl gnupg -y
-#     sudo install -m 0755 -d /etc/apt/keyrings
-#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#     sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-#     # Add the repository to Apt sources:
-#     echo \
-#     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#     sudo apt-get update
-#     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-# fi
-
-# command -v screen &> /dev/null
-
-# if [ $? -eq 0 ]; then
-#   echo "Copy successful"
-# else
-#    sudo apt-get update
-#    sudo apt install screen -y
-# fi
-   
-# sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-# sudo docker compose down
-# sudo docker compose up --build -d
-# sudo docker system prune -f
-
-sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt-get install -y libuv1-dev
-# sudo ufw enable; sudo ufw allow 10000/tcp; sudo ufw allow 20000/tcp; sudo ufw allow 22/tcp; sudo ufw status verbose;
+## Compose
+sudo docker compose down
+sudo docker compose up --build -d
